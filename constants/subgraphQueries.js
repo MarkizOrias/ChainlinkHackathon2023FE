a446173bf3b0631d3e15db8a421901c86ec2551f
MarkizOrias <georgi.bargan@gmail.com>
2023-06-06 21:57:18 +0200

    new front end

---
+++ constants/subgraphQueries.js
@@ -0,0 +1,48 @@
+import { gql } from "@apollo/client"
+
+const GET_ACTIVE_ITEMS = gql`
+{
+  nftMinteds(orderBy: tokenId) {
+    tokenId
+    id
+    minter
+    blockTimestamp
+  }
+  nftSetTokenURIs(orderBy: tokenId) {
+    uri
+  }
+  nftAuctionTimeUpdateds(orderBy: tokenId) {
+    tokenId
+    time
+    blockTimestamp
+  }
+  nftBidPlaceds(orderBy: tokenId) {
+    bidder
+    amount
+    tokenId
+  }
+  transfers(
+    orderBy: tokenId
+    where: {from_not_contains: "0x0000000000000000000000000000000000000000"}
+  ) {
+    tokenId
+  }
+  nftWithdrawCompleteds(orderBy: tokenId) {
+    tokenId
+  }
+  nftAddedPendingBidsForWithdrawals(orderBy: blockTimestamp) {
+    bid
+    bidder
+    blockTimestamp
+  }
+  nftPendingBidsWithdrawals(orderBy: blockTimestamp) {
+    bid
+    bidder
+    blockTimestamp
+  }
+}
+`
+
+export default GET_ACTIVE_ITEMS
+
+
